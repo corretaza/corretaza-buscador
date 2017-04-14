a98479b0b3422de92088f4511ed1511e3cf7e391
Roger Camargo <huogerac@gmail.com>
2017-04-14 11:05:56 -0300

    #UX Atualizado model de clientes

---
+++ seucorretor/crm/migrations/0003_auto_20170414_1105.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crm', '0002_atendimento_urgencia'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='atendimento',
+            name='urgencia',
+            field=models.CharField(default='9', max_length=2, verbose_name='Urgencia', choices=[('0', 'Muito urgente, preciso achar um im\xf3vel em menos de 1 semana/mes'), ('1', 'Urgente, preciso achar um im\xf3vel dentro de 3 meses'), ('2', 'N\xe3o urgente, estou pesquisando para comprar daqui 6 meses ou 1 ano'), ('3', 'Investidor, quero o melhor custo/benef\xedcio'), ('9', 'N\xe3o informado')]),
+            preserve_default=True,
+        ),
+    ]
