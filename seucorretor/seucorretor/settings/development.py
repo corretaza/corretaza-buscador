6aff6c221fa69006380cdc030909a3ad5d7f86fc
Roger Camargo <huogerac@gmail.com>
2017-11-05 11:00:19 -0200

    Revert "Adicionado django-debug-toolbar"
    This reverts commit 789d8479611d5e93b6e4a70923e0549b9fcd1593.

--- seucorretor/seucorretor/settings/development.py
+++ seucorretor/seucorretor/settings/development.py
@@ -27,13 +27,13 @@ DATABASES = {
 }
 
 # django-debug-toolbar
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
+#INSTALLED_APPS += (
+#    'debug_toolbar',
+#)
 
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
+#MIDDLEWARE_CLASSES += (
+#    'debug_toolbar.middleware.DebugToolbarMiddleware',
+#)
 
 DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
